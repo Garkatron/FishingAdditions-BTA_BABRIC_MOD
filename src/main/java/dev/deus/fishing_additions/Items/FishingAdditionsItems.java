0a7c257c646df18f781902a68a1d0f2d23bc11a0
Garkatron <145294535+Garkatron@users.noreply.github.com>
2024-06-05 22:36:47 +0200

    0.1.14:223605062024/@Garkatron Added: pufferfish/&cockeed salmon/&coocked tropical_fish

--- src/main/java/dev/deus/fishing_additions/Items/FishingAdditionsItems.java
+++ src/main/java/dev/deus/fishing_additions/Items/FishingAdditionsItems.java
@@ -2,6 +2,7 @@ package dev.deus.fishing_additions.Items;
 
 import dev.deus.fishing_additions.Items.ItemModel.HydraScale;
 import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemFood;
 import turniplabs.halplibe.helper.ItemBuilder;
 
 import static dev.deus.fishing_additions.FishingAdditions.MOD_ID;
@@ -14,12 +15,21 @@ public class FishingAdditionsItems {
 	public static Item iron_fishing_rod_item;
 	public static Item steel_fishing_rod_item;
 
+	public static ItemFood pufferfish;
+	public static ItemFood salmon;
+	public static ItemFood tropical_fish;
+
+	public static ItemFood cooked_salmon;
+	public static ItemFood cooked_cod;
+
+
+
 	// Ingredients
 	public static Item HydraScale;
 
 
 	public void Initialize() {
-		//MOD_ID, new ItemGoldFishingRod("GoldFishingRod", config.newItemID()), ,"gold_fishing_rod.png"
+
 		gold_fishing_rod_item = new ItemBuilder(MOD_ID)
 			.build(new ItemGoldFishingRod("gold_fishing_rod", config.newItemID()));
 
@@ -28,6 +38,56 @@ public class FishingAdditionsItems {
 
 		steel_fishing_rod_item = new ItemBuilder(MOD_ID)
 			.build(new ItemSteelFishingRod("steel_fishing_rod", config.newItemID()));
+
+		//
+		pufferfish = new ItemBuilder(MOD_ID)
+			.build(new ItemFood(
+				"pufferfish",
+				config.newItemID(),
+				1,
+				2,
+				false,
+				64
+			));
+
+		salmon = new ItemBuilder(MOD_ID)
+			.build(new ItemFood("salmon", config.newItemID(),
+				2, // Valor de saturación para salmon
+				4, // Valor de curación para salmon
+				false,
+				64
+			));
+
+		tropical_fish = new ItemBuilder(MOD_ID)
+			.build(new ItemFood(
+				"tropical_fish", config.newItemID(),
+				1,
+				3,
+				false,
+				64
+			));
+
+		cooked_salmon = new ItemBuilder(MOD_ID)
+			.build(new ItemFood(
+				"cooked_salmon", config.newItemID(),
+				6,
+				8,
+				false,
+				64
+			));
+
+		cooked_cod = new ItemBuilder(MOD_ID)
+			.build(new ItemFood(
+				"cooked_cod",
+				config.newItemID(),
+				5,
+				7,
+				false,
+				64
+			));
+
+
+
 
 		HydraScale = new ItemBuilder(MOD_ID)
 			.build(new HydraScale("hydra_scale", config.newItemID()));
